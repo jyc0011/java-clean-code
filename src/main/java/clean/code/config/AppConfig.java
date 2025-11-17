04297c4bb53092a7e54d9fa7e3a102b3b9205a3f
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-17 23:06:58 +0900

    feat : 의존성 주입

--- src/main/java/clean/code/config/AppConfig.java
+++ src/main/java/clean/code/config/AppConfig.java
@@ -4,18 +4,26 @@ import clean.code.core.Analyzer;
 import clean.code.core.CodeCheckRunner;
 import clean.code.core.ProjectScanner;
 import clean.code.report.ConsoleReporter;
+import clean.code.rules.Rule;
+import clean.code.rules.RuleRegistry;
+import java.util.List;
 
 /**
  * 애플리케이션의 의존성(객체)을 생성하고 주입
  */
 public class AppConfig {
+
+    public RuleRegistry ruleRegistry() {
+        return new RuleRegistry();
+    }
+
     public ProjectScanner projectScanner() {
         return new ProjectScanner();
     }
 
     public Analyzer analyzer() {
-        // TODO: 규칙(Rule) 리스트를 생성하여 Analyzer에 주입
-        return new Analyzer();
+        List<Rule> activeRules = ruleRegistry().getActiveRules();
+        return new Analyzer(activeRules);
     }
 
     public ConsoleReporter consoleReporter() {
